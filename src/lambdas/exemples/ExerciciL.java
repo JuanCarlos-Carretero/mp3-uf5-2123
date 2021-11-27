430b93ba50a33f8fc91a133c0099b89fba6a3087
jherna <jherna76@elpuig.xeill.net>
2021-11-27 12:41:18 +0100

    exercici streams

--- src/lambdas/exemples/ExerciciL.java
+++ src/lambdas/exemples/ExerciciL.java
@@ -18,8 +18,11 @@ public class ExerciciL {
         Persona p6 = new Persona("Jaime", Persona.Genere.HOME, LocalDate.parse("24/04/1979",format));
         Persona p7 = new Persona("Khal", Persona.Genere.HOME, LocalDate.parse("10/08/1979",format));
         Persona p8 = new Persona("Daenerys", Persona.Genere.DONA, LocalDate.parse("12/11/1992",format));
+        Persona p9 = new Persona("Davos", Persona.Genere.HOME, LocalDate.parse("12/11/1965",format));
+        Persona p10 = new Persona("Jon Neu", Persona.Genere.HOME, LocalDate.parse("12/11/1986",format));
+        Persona p11 = new Persona("Brienne", Persona.Genere.DONA, LocalDate.parse("12/11/1989",format));
 
-        Persona[] lpers = {p1,p2,p3,p4,p5,p6,p7,p8};
+        Persona[] lpers = {p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11};
         List<Persona> llista_persones;
         llista_persones = new ArrayList<>(Arrays.asList(lpers));
         Map<Integer,Integer> mapPersones = new HashMap<>();
@@ -89,6 +92,17 @@ public class ExerciciL {
         // 9 - Llistat dels dos HOMES més joves (stream)
 
         // 10- Esborrar (no filtrar o imprimir) del llistat les persones entre 30 i 40 anys (amb lambda)
+
+        // 11 - Persones que tinguin una 'a' al seu nom
+        System.out.println("\n11 Amb una 'A'");
+
+        //12 - Llistat de les dates de naixament + dos dies
+        System.out.println("\n12 - dates amb dos dies més");
+
+
+        //13 - Rejovenir dos anys a totes les persones
+        System.out.println("\n13 - Rejovenir dos anys a totes les persones");
+
 
     }
 
